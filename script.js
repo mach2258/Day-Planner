1bd7f25c05eebd3386bddbe0698aac12b9ae47dc
Erich Elste <erichelste@yahoo.com>
2022-04-18 00:30:54 -0400

    layout finished

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 
+var eventList = document.getElementsByClassName("saveBtn")
+
 var today = moment();
-$("#currentday").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var hour = moment().format('H');
+console.log(hour)
+
+$('.description').each(function () {
+    // Find the number ID
+    var idEl = $(this).attr("id")
+    console.log($(this).attr("id"))
+    // If statement for matching the time
+    if (hour == idEl) {
+        $(this).addClass("present")
+    } else if (hour > idEl) {
+        $(this).addClass("past")
+    } else {
+        $(this).addClass("future")
+    }
+    // If statement for if its ahead
+    // If for if its behind
+})
+
+var events = [];
+
+function storeEvents() {
+    localStorage.setItem("events", JSON.stringify(events));
+    console.log(storeEvents())
+}
+
+eventList.addEventListener("click", function(event) {
+    var element = event.target
+
+
+});
